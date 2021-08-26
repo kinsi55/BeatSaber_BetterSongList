73267110c388ad8f2b053b3957ce9335de246438
Kinsi <kinsi55@users.noreply.github.com>
2021-08-27 00:09:33 +0200

    Doesnt work for playlists and honestly the previous solution is better for compat

--- Util/BeatmapsUtil.cs
+++ Util/BeatmapsUtil.cs
@@ -1,10 +1,13 @@
 ﻿namespace BetterSongList.Util {
 	static class BeatmapsUtil {
 		public static string GetHashOfPreview(IPreviewBeatmapLevel preview) {
-			if(preview is CustomPreviewBeatmapLevel)
-				return preview.levelID.Substring(13, 40);
+			if(preview.levelID.Length < 53)
+				return null;
 
-			return null;
+			if(preview.levelID[12] != '_') // custom_level_<hash, 40 chars>
+				return null;
+
+			return preview.levelID.Substring(13, 40);
 		}
 
 		public static int GetCharacteristicFromDifficulty(IDifficultyBeatmap diff) {
