38e6924fe6b5731521aa50bdcee9144dc1f168e8
Kinsi <kinsi55@users.noreply.github.com>
2021-08-26 20:21:47 +0200

    Clear our filter when basegame filter clear is called by other plugins

--- HarmonyPatches/HookFilterClear.cs
+++ HarmonyPatches/HookFilterClear.cs
@@ -1,4 +1,16 @@
-﻿namespace BetterSongList.HarmonyPatches {
-	class HookFilterClear {
+﻿using BetterSongList.UI;
+using HarmonyLib;
+using System.Reflection;
+
+namespace BetterSongList.HarmonyPatches {
+	[HarmonyPatch(typeof(LevelSearchViewController), nameof(LevelSearchViewController.ResetCurrentFilterParams))]
+	static class HookFilterClear {
+		static void Prefix() {
+			// Compat for stuff like SRM mods so that we clear our filter as well when they request a basegame filter clear
+			if(Assembly.GetCallingAssembly().GetName().Name == "Main")
+				return;
+
+			FilterUI.SetFilter(null, false, false);
+		}
 	}
 }
