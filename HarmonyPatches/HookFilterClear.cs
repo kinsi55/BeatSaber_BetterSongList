e9e5e44c0043252620d6dc8b4af29c5486d1db40
Kinsi <kinsi55@users.noreply.github.com>
2021-09-01 01:03:53 +0200

    Hook filter clear bug

--- HarmonyPatches/HookFilterClear.cs
+++ HarmonyPatches/HookFilterClear.cs
@@ -1,15 +1,24 @@
 ﻿using BetterSongList.UI;
 using HarmonyLib;
+using System.Diagnostics;
 using System.Reflection;
+using System.Linq;
 
 namespace BetterSongList.HarmonyPatches {
 	[HarmonyPatch(typeof(LevelSearchViewController), nameof(LevelSearchViewController.ResetCurrentFilterParams))]
 	static class HookFilterClear {
+		[HarmonyPriority(int.MaxValue)]
 		static void Prefix() {
+			// GetCallingAssembly isnt good enough because of Harmony patches
+			var x = new StackTrace().GetFrames().DefaultIfEmpty(null).ElementAtOrDefault(2)?.GetMethod().DeclaringType.Assembly.GetName().Name;
+
 			// Compat for stuff like SRM mods so that we clear our filter as well when they request a basegame filter clear
-			if(Assembly.GetCallingAssembly().GetName().Name == "Main")
+			if(x == "Main")
 				return;
 
+#if DEBUG
+			Plugin.Log.Debug(string.Format("HookFilterClear(): Filter clear requested from other assembly ({0})", Assembly.GetCallingAssembly().GetName()));
+#endif
 			FilterUI.SetFilter(null, false, false);
 		}
 	}
