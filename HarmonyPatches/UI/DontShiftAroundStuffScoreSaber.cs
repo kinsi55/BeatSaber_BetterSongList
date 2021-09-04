6e9447b76de49dacf0b208eba909c02e6eb74714
Kinsi <kinsi55@users.noreply.github.com>
2021-09-04 14:15:56 +0200

    Un-Scuf ScoreSaber logic

---
+++ HarmonyPatches/UI/DontShiftAroundStuffScoreSaber.cs
@@ -0,0 +1,31 @@
+﻿using HarmonyLib;
+using HMUI;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace BetterSongList.HarmonyPatches.UI {
+	[HarmonyPatch]
+	class DontShiftAroundStuffScoreSaber {
+		static MethodBase TargetMethod() => AccessTools.Method("ScoreSaber.UI.Other.ScoreSaberLeaderboardView:SetPlayButtonState");
+		static Exception Cleanup(Exception ex) => null;
+
+		[HarmonyPriority(int.MinValue)]
+		// taking state by ref because "the first prefix that returns false will skip all remaining prefixes [..with..] ref arguments"
+		// https://harmony.pardeike.net/articles/patching-prefix.html
+		static bool Prefix(ref bool state, Button ____playButton) {
+			var l = ((RectTransform)____playButton.gameObject.transform).localScale;
+
+			l.x = state ? 1 : 0;
+			____playButton.GetComponent<NoTransitionsButton>().enabled = state;
+			((RectTransform)____playButton.gameObject.transform).localScale = l;
+			return false;
+		}
+	}
+}
