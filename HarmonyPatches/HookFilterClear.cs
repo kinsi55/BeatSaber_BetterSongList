8b1a06953d288bef7ea30a817a1a67b04b665e57
Kinsi <kinsi55@users.noreply.github.com>
2021-09-05 17:24:21 +0200

    Fix funny basegame(?) bug

--- HarmonyPatches/HookFilterClear.cs
+++ HarmonyPatches/HookFilterClear.cs
@@ -16,7 +16,7 @@ namespace BetterSongList.HarmonyPatches {
 				return;
 
 #if DEBUG
-			Plugin.Log.Debug(string.Format("HookFilterClear(): Filter clear requested from other assembly ({0})", Assembly.GetCallingAssembly().GetName()));
+			Plugin.Log.Debug(string.Format("HookFilterClear(): Filter clear requested from other assembly ({0})", System.Reflection.Assembly.GetCallingAssembly().GetName()));
 #endif
 			FilterUI.SetFilter(null, false, false);
 		}
