25472f42c52f6f7f30ad3d1b3986837e2f830237
Kinsi <kinsi55@users.noreply.github.com>
2021-09-10 20:34:24 +0200

    Replicating others bugs for interop lol

--- HarmonyPatches/UI/DontShiftAroundStuffScoreSaber.cs
+++ HarmonyPatches/UI/DontShiftAroundStuffScoreSaber.cs
@@ -15,6 +15,10 @@ namespace BetterSongList.HarmonyPatches.UI {
 		// taking state by ref because "the first prefix that returns false will skip all remaining prefixes [..with..] ref arguments"
 		// https://harmony.pardeike.net/articles/patching-prefix.html
 		static bool Prefix(ref bool state, Button ____playButton) {
+			// Scoresaber will use the wrong play button in certain cases.... so I guess I gotta handle that?
+			if(____playButton.transform.parent?.parent?.parent?.parent?.name != "LevelCollectionNavigationController")
+				return true;
+
 			var l = ((RectTransform)____playButton.gameObject.transform).localScale;
 
 			l.y = l.x = state ? 1 : 0.001f;
