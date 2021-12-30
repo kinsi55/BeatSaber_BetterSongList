90ff4dad87d93c6346b13f3bd48ba61ae023b779
Kinsi <kinsi55@users.noreply.github.com>
2021-12-31 00:52:37 +0100

    Yeah super simple problem absolutely no bugs

--- Util/BeatmapPatternDetection.cs
+++ Util/BeatmapPatternDetection.cs
@@ -12,34 +12,36 @@ namespace BetterSongList.Util {
 
 			var wallExistence = new float[2];
 
-			foreach(var x in beatmapData.beatmapLinesData) {
-				foreach(var bme in x.beatmapObjectsData) {
-					if(bme.beatmapObjectType != BeatmapObjectType.Obstacle)
-						continue;
+			foreach(var bme in beatmapData
+				.beatmapLinesData
+				.SelectMany(x => x.beatmapObjectsData)
+				.Where(x => x.beatmapObjectType == BeatmapObjectType.Obstacle && x.lineIndex != 3)
+				.OrderBy(x => x.time)
+			) {
+				var o = (ObstacleData)bme;
 
-					var o = (ObstacleData)bme;
+				// Ignore 1 wide walls on left
+				if(o.lineIndex == 1 && o.width == 1)
+					continue;
 
-					// Filter out fake walls, they dont drain energy
-					if(o.duration < 0 || o.width <= 0)
-						continue;
+				// Filter out fake walls, they dont drain energy
+				if(o.duration < 0 || o.width <= 0)
+					continue;
 
-					// Detect >2 wide walls anywhere, or 2 wide wall in middle
-					if(o.obstacleType == ObstacleType.Top && (o.width > 2 || (o.width == 2 && o.lineIndex == 1)))
-						return true;
+				// Detect >2 wide walls anywhere, or 2 wide wall in middle
+				if(o.obstacleType == ObstacleType.Top && (o.width > 2 || (o.width == 2 && o.lineIndex == 1)))
+					return true;
 
-					// Skip 1 wide walls on outer lanes
-					if(o.width == 1 && (o.lineIndex == 1 || o.lineIndex == 4))
-						continue;
+				// Is the wall on the left or right half?
+				var isLeftHalf = o.lineIndex <= 1;
 
-					// Is the wall on the left or right half?
-					var isLeftHalf = o.lineIndex <= 2;
+				// Check if the other half has an active wall, which would mean there is one on both halfs
+				// I know this technically does not check if one of the halves is half-height, but whatever
+				// Extend wall lengths by 200ms so that staggered crouchwalls that dont overlap are caught
+				if(wallExistence[isLeftHalf ? 1 : 0] + 0.2f >= o.time)
+					return true;
 
-					// Check if the other half has an active wall, which would mean there is one on both halfs
-					if(wallExistence[isLeftHalf ? 1 : 0] >= o.time)
-						return true;
-
-					wallExistence[isLeftHalf ? 0 : 1] = Math.Max(wallExistence[isLeftHalf ? 0 : 1], o.time + o.duration);
-				}
+				wallExistence[isLeftHalf ? 0 : 1] = Math.Max(wallExistence[isLeftHalf ? 0 : 1], o.time + o.duration);
 			}
 			return false;
 		}
