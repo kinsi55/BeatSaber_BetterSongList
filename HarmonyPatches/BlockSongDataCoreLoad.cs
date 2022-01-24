034b536c35e4b2bc1f5a81b14c07dfc9bd6b6bd1
Kinsi <kinsi55@users.noreply.github.com>
2022-01-24 23:15:29 +0100

    I am tired

---
+++ HarmonyPatches/BlockSongDataCoreLoad.cs
@@ -0,0 +1,16 @@
+﻿using HarmonyLib;
+using System;
+using System.Reflection;
+
+namespace BetterSongList.HarmonyPatches {
+	[HarmonyPatch]
+	static class BlockSongDataCoreLoad {
+		public static bool doBlock = false;
+		static bool Prefix() => !doBlock;
+
+		static MethodBase TargetMethod() => IPA.Loader.PluginManager.GetPluginFromId("SongDataCore")?
+			.Assembly.GetType("SongDataCore.Plugin")?
+			.GetMethod("LoadDatabases", BindingFlags.Instance | BindingFlags.NonPublic);
+		static Exception Cleanup(Exception ex) => null;
+	}
+}
