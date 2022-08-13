7e8839246456743242f4dff47d70b5e60a994c79
Kinsi <kinsi55@users.noreply.github.com>
2022-08-13 18:35:23 +0200

    Better

--- HarmonyPatches/RestoreLevelSelection.cs
+++ HarmonyPatches/RestoreLevelSelection.cs
@@ -8,7 +8,12 @@ using System.Reflection;
 namespace BetterSongList.HarmonyPatches {
 	[HarmonyPatch(typeof(LevelSelectionFlowCoordinator), "DidActivate")]
 	static class LevelSelectionFlowCoordinator_DidActivate {
-		static readonly ConstructorInfo thingy = AccessTools.FirstConstructor(typeof(LevelSelectionFlowCoordinator.State), x => !x.IsPublic);
+		static readonly ConstructorInfo thingy = AccessTools.FirstConstructor(typeof(LevelSelectionFlowCoordinator.State), x => x.GetParameters().Length == 4);
+		// Why this isnt publicly accessible through a method? Nobody fucking knows.
+		static readonly IPA.Utilities.FieldAccessor<BeatmapLevelsModel, Dictionary<string, IPreviewBeatmapLevel>>.Accessor BeatmapLevelsModel_loadedPreviewBeatmapLevels =
+			IPA.Utilities.FieldAccessor<BeatmapLevelsModel, Dictionary<string, IPreviewBeatmapLevel>>.GetAccessor("_loadedPreviewBeatmapLevels");
+
+		static BeatmapLevelsModel beatmapLevelsModel = UnityEngine.Object.FindObjectOfType<BeatmapLevelsModel>();
 
 		static void Prefix(ref LevelSelectionFlowCoordinator.State ____startState) {
 			if(____startState != null) {
@@ -21,23 +26,14 @@ namespace BetterSongList.HarmonyPatches {
 			if(!Enum.TryParse(Config.Instance.LastCategory, out SelectLevelCategoryViewController.LevelCategory restoreCategory))
 				restoreCategory = SelectLevelCategoryViewController.LevelCategory.None;
 
-			var restoreLevel = Config.Instance.LastSong;
+			if(!BeatmapLevelsModel_loadedPreviewBeatmapLevels(ref beatmapLevelsModel).TryGetValue(Config.Instance.LastSong, out var m))
+				m = null;
 
-			IPreviewBeatmapLevel m = null;
-
-			foreach(var x in SongCore.Loader.BeatmapLevelsModelSO.allLoadedBeatmapLevelPackCollection.beatmapLevelPacks) {
-				foreach(var y in x.beatmapLevelCollection.beatmapLevels) {
-					if(y.levelID != restoreLevel)
-						continue;
-
-					m = y;
-					break;
-				}
-			}
+			var packId = Config.Instance.LastPack == null ? null : PlaylistsUtil.GetPack(Config.Instance.LastPack)?.packID;
 
 			____startState = (LevelSelectionFlowCoordinator.State)thingy.Invoke(new object[] {
 				restoreCategory,
-				new BeatmapLevelPack(PlaylistsUtil.GetPack(Config.Instance.LastPack)?.packID, null, null, null, null, null),
+				packId == null ? null : new BeatmapLevelPack(packId, null, null, null, null, null),
 				m,
 				null
 			});
