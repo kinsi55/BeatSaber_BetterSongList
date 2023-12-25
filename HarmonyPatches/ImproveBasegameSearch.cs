c279ac110cc05911e85cc44c621e162126bb33b2
Meivyn <793322+Meivyn@users.noreply.github.com>
2023-12-25 04:10:33 -0500

    Restore search by level author

--- HarmonyPatches/ImproveBasegameSearch.cs
+++ HarmonyPatches/ImproveBasegameSearch.cs
@@ -14,5 +14,47 @@ namespace BetterSongList.HarmonyPatches {
 			if(searchTerms.Length > 1)
 				searchTerms = new string[] { string.Join(" ", searchTerms) };
 		}
+
+		static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions, ILGenerator il) {
+			if(!Config.Instance.ModBasegameSearch)
+				return instructions;
+
+			// This appends a space and the levelAuthorName to the levelString variable.
+			var matcher = new CodeMatcher(instructions)
+				.MatchForward(true,
+					new CodeMatch(OpCodes.Ldloc_2, null, "L_previewBeatmapLevel"),
+					new CodeMatch(),
+					new CodeMatch(OpCodes.Stelem_Ref),
+					new CodeMatch(x => x.opcode == OpCodes.Call && (x.operand as MethodInfo)?.Name == nameof(string.Concat), "Call_Concat")
+				);
+
+			var text = il.DeclareLocal(typeof(string));
+
+			matcher.Advance(1).Insert(
+				new CodeInstruction(OpCodes.Stloc, text),
+				new CodeInstruction(OpCodes.Ldc_I4_3),
+				new CodeInstruction(OpCodes.Newarr, typeof(string)),
+
+				new CodeInstruction(OpCodes.Dup),
+				new CodeInstruction(OpCodes.Ldc_I4_0),
+				new CodeInstruction(OpCodes.Ldloc, text),
+				new CodeInstruction(OpCodes.Stelem_Ref),
+
+				new CodeInstruction(OpCodes.Dup),
+				new CodeInstruction(OpCodes.Ldc_I4_1),
+				new CodeInstruction(OpCodes.Ldstr, " "),
+				new CodeInstruction(OpCodes.Stelem_Ref),
+
+				new CodeInstruction(OpCodes.Dup),
+				new CodeInstruction(OpCodes.Ldc_I4_2),
+				matcher.NamedMatch("L_previewBeatmapLevel"),
+				new CodeInstruction(OpCodes.Callvirt, AccessTools.PropertyGetter(typeof(IPreviewBeatmapLevel), nameof(IPreviewBeatmapLevel.levelAuthorName))),
+				new CodeInstruction(OpCodes.Stelem_Ref),
+
+				matcher.NamedMatch("Call_Concat")
+			);
+
+			return matcher.InstructionEnumeration();
+		}
 	}
 }
